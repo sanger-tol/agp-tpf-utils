8bdf90bfafd247ff0987ba1d076e785d2eadf614
James Gilbert <jgrg@sanger.ac.uk>
2024-10-29 17:48:45 +0000

    Buffering to reduce number of calls to re.finditer()

--- src/tola/fasta/index.py
+++ src/tola/fasta/index.py
@@ -53,7 +53,7 @@ class FastaInfo:
         return s.getvalue()
 
 
-def index_fasta_bytes(file: Path, buffer_size: int = 10e6):
+def index_fasta_file(file: Path, buffer_size: int = 10_000_000):
     name = None
     seq_length = None
     file_offset = None
@@ -66,6 +66,44 @@ def index_fasta_bytes(file: Path, buffer_size: int = 10e6):
 
     info = []
 
+    def load_info():
+        process_seq_buffer()
+        if region_end:
+            seq_regions.append((region_start + 1, region_end))
+        info.append(
+            FastaInfo(
+                name,
+                seq_length,
+                file_offset,
+                residues_per_line,
+                residues_per_line + line_end_bytes,
+                seq_regions,
+            )
+        )
+
+    def process_seq_buffer():
+        nonlocal seq_length
+        nonlocal region_start
+        nonlocal region_end
+        seq_bytes = seq_buffer.getvalue()
+        seq_buffer.seek(0)
+        seq_buffer.truncate(0)
+
+        # Treat any non-ACGT character as an "N" (i.e. gap)
+        for m in re.finditer(rb"[ACGTacgt]+", seq_bytes):
+            start = seq_length + m.start()
+            end = seq_length + m.end()
+            if start == region_end:
+                region_end = end
+            else:
+                if region_end:
+                    seq_regions.append((region_start + 1, region_end))
+                region_start = start
+                region_end = end
+
+        seq_length += len(seq_bytes)
+
+
     # Opening the file in bytes mode means that Windows ("\r\n") or UNIX
     # ("\n") line endings are preserved.  It is also about 10% faster than
     # decoding to UTF-8.
@@ -76,18 +114,7 @@ def index_fasta_bytes(file: Path, buffer_size: int = 10e6):
                 # If this isn't the first sequence in the file, store the
                 # accumulated data from the previous sequence.
                 if name:
-                    if region_end:
-                        seq_regions.append((region_start + 1, region_end))
-                    info.append(
-                        FastaInfo(
-                            name,
-                            seq_length,
-                            file_offset,
-                            residues_per_line,
-                            residues_per_line + line_end_bytes,
-                            seq_regions,
-                        )
-                    )
+                    load_info()
 
                 # Get new name by splitting on whitespace beyond the first
                 # character and taking the first element of the array. This
@@ -105,7 +132,6 @@ def index_fasta_bytes(file: Path, buffer_size: int = 10e6):
                 region_end = None
                 seq_regions = []
 
-
                 # The first residue of the sequence will be where the file
                 # pointer now is.
                 file_offset = fh.tell()
@@ -115,44 +141,24 @@ def index_fasta_bytes(file: Path, buffer_size: int = 10e6):
                 # second to last byte will be ord("\r") == 13
                 line_end_bytes = 2 if line[-2] == 13 else 1
             else:
-                residues = len(line) - line_end_bytes
-                if residues > residues_per_line:
-                    residues_per_line = residues
+                if not residues_per_line:
+                    residues_per_line = len(line) - line_end_bytes
 
-                # Treat any non-ACGT character as an "N" (i.e. gap)
-                for m in re.finditer(rb"[ACGTacgt]+", line[:-line_end_bytes]):
-                    start = seq_length + m.start()
-                    end = seq_length + m.end()
-                    if start == region_end:
-                        region_end = end
-                    else:
-                        if region_end:
-                            seq_regions.append((region_start + 1, region_end))
-                        region_start = start
-                        region_end = end
+                seq_buffer.write(line[:-line_end_bytes])
+                if seq_buffer.tell() > buffer_size:
+                    process_seq_buffer()
 
-                seq_length += residues
+    # Store info for the last sequence in the file
     if name:
-        if region_end:
-            seq_regions.append((region_start + 1, region_end))
-        info.append(
-            FastaInfo(
-                name,
-                seq_length,
-                file_offset,
-                residues_per_line,
-                residues_per_line + line_end_bytes,
-                seq_regions,
-            )
-        )
+        load_info()
 
     return info
 
 
 if __name__ == "__main__":
     for file in sys.argv[1:]:
-        info = index_fasta_bytes(Path(file))
+        info = index_fasta_file(Path(file))
         for fst in info:
-            sys.stdout.write("\n")
+            # sys.stdout.write("\n")
             sys.stdout.write(fst.fai_row())
-            sys.stdout.write(fst.regions())
+            # sys.stdout.write(fst.regions())
--- tests/fasta_test.py
+++ tests/fasta_test.py
@@ -2,23 +2,23 @@ import pathlib
 
 import pytest
 
-from tola.fasta.index import index_fasta_bytes
+from tola.fasta.index import index_fasta_file
 
 
 def list_fasta_files():
     fasta_dir = pathlib.Path(__file__).parent / "fasta"
     for ff in fasta_dir.iterdir():
-        if ff.suffix == '.fa':
+        if ff.suffix == ".fa":
             yield ff
 
 
 @pytest.mark.parametrize("fasta_file", list_fasta_files())
 def test_fai(fasta_file):
     fai_file = pathlib.Path(str(fasta_file) + ".fai")
-    fai_str = fai_file.read_text()
     if not fai_file.exists():
         msg = f"Missing expected '.fai' file: {fai_file}"
         raise ValueError(msg)
-    info = index_fasta_bytes(fasta_file)
+    fai_str = fai_file.read_text()
+    info = index_fasta_file(fasta_file)
     test_str = "".join(x.fai_row() for x in info)
     assert test_str == fai_str
